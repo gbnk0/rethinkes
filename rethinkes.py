f901a283b87a6c19b33da22b3838585b7d38b2ba
gbnk0 <michael.amara2@gmail.com>
2018-02-06 17:52:42 +0100

    Add argparse

--- rethinkes.py
+++ rethinkes.py
@@ -1,14 +1,24 @@
 
 import json
+import argparse
 import rethinkdb as r
 from elasticsearch import Elasticsearch
 
-rdbhost = "localhost"
+parser = argparse.ArgumentParser()
+parser.add_argument('--keep-id', help='Keeps the rethinkdb document ID in elasticsearch', action='store_true')
+parser.add_argument('--rdbhost', help='Set the rethinkdb source host', default='localhost')
+parser.add_argument('--eshost', help='Set the elasticsearch dest hosts (comma separated if multiple)', default='localhost')
+parser.add_argument('--doctype', help='Set the doctype in ES (comma separated if multiple)', default='doc')
+parser.add_argument('--tables', help='Choose the tables to replicate (comma separated if multiple)', default='test')
+parser.add_argument('--database', help='Set the database name', default='test')
+args = parser.parse_args()
+
+rdbhost = args.rdbhost
 rdbport = 28015
-database = "backend"
-tables = ["posts"]
-hosts = ["localhost"]
-doctype = "post"
+database = args.database
+tables = (args.tables).split(',')
+hosts = (args.eshost).split(',')
+doctype = args.doctype
 
 r.connect(rdbhost, rdbport).repl()
 
@@ -22,6 +32,10 @@ for table in tables:
     cursor = r.db(database).table(table).run()
 
     for doc in cursor:
-        print("INSERT: ", doc)
-        res = es.index(index=table, doc_type=doctype, id=doc['id'], body=doc)
+        options = {}
         
+        if args.keep_id:
+            options['id'] = doc['id']
+
+        res = es.index(index=table, doc_type=doctype, body=doc, **options)
+        print("INSERT: ", doc)
